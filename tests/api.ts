81a374a9c1e16ce2f9b241b88c6a3af98c680837
ErikMCM <70036542+ErikMCM@users.noreply.github.com>
2023-09-12 17:36:31 -0500

    API Test

---
+++ tests/api.ts
@@ -0,0 +1,17 @@
+Bun.serve({
+    fetch(req: Request) {
+        const url = new URL(req.url);
+
+        switch (url.pathname) {
+            case "/":
+                return new Response("Hello World!");
+            case "/json":
+                return new Response(JSON.stringify({ hello: "world" }), {
+                    headers: { "content-type": "application/json" }
+                });
+            default:
+                return new Response(`Sorry, I didn't find ${url.pathname}`, { status: 404 });
+        }        
+    },
+    port: 8080
+});
